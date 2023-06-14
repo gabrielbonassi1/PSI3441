482e02c263851dece221784f509e9c52859e7d0d
gabrielbonassi1 <gabriel.bonassi@hotmail.com>
2023-06-14 09:17:04 -0300

    Ex8 teórico - final

--- ex8.c
+++ ex8.c
@@ -12,22 +12,22 @@ int main(void) {
 
     while (1)
     {
-        ADC0->SC1[0] = 0;
+        ADC0->SC1[0] = 0x10; // inicia a conversao, single-ended, AD8 selecionado como input
 
-        while (!(ADC0->SC1[0] & 0x80)){}
+        while (!(ADC0->SC1[0] & 0x80)){} //aguarda a conversao acabar (faco um AND entre a flag COCO e 1, quando os 2 forem 1, retorna 1 e para o while)
 
-        result = ADC0->R[0];
-        LED_set(result >> 7);
+        result = ADC0->R[0]; // le o resultado da conversao na var result
+        LED_set(result >> 7); // seta o led com base no bit 7 do result
         
     }
     
 }
 
 void ADC0_init(void) {
-    SIM->SCGC5 |= 0x2000; // enable clock PORTE (pg. 206)
+    SIM->SCGC5 |= (1<<10); // enable clock PORTB (pg. 206)
     SIM->SCGC6 |= 0x8000000; // enable clock ADC0 (pg. 207)
-    PORTE->PCR[30] = 0; // enable PTE30 pin out
-    ADC0->SC2 &= -0x40; // software trigger
+    PORTE->PCR[0] = 0; // enable PTB0 pin out
+    ADC0->SC2 &= ~0x40; // software trigger
     ADC0->CFG1 = 0x54;
 }
 
@@ -46,19 +46,19 @@ void LED_init(void) {
 
 void LED_set(int s) {
     // Red LED
-    if (s & 1) { // use BIT 0 of s
+    if (s & 1) { // usa BIT 0 de s
         PTB->PCOR = 0x40000; // turn on
     } else {
         PTB->PSOR = 0x40000; // turn off
     }
     // Green LED
-    if (s & 2) { //use BIT 1 of s
+    if (s & 2) { //usa BIT 1 do s
         PTB->PCOR = 0x80000; // turn on
     } else {
         PTB->PSOR = 0x40000; // turn off
     }
     // Blue LED
-    if (s & 4) {
+    if (s & 4) { //usa bit 2 do s
         PTD->PCOR = 0x02; // turn on
     } else {
         PTD->PSOR = 0x02; // turn off
