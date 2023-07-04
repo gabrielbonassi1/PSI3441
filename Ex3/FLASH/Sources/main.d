b8b0df0eaf9cfdaf7b0cb18d23857c04a40e8795
gabrielbonassi1 <gabriel.bonassi@hotmail.com>
2023-07-04 11:35:50 -0300

    Ex3

---
+++ Ex3/FLASH/Sources/main.d
@@ -0,0 +1,79 @@
+Sources/main.o: ../Sources/main.c \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/Cpu.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Types.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Error.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Const.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/IO_Map.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/iofiles/MKL25Z4.h \
+ ../Sources/Events.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Types.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Error.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Const.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/IO_Map.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/SysTick.h \
+ C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/Cpu.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/SysTick_PDD.h \
+ C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/Cpu.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Types.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Error.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Const.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/IO_Map.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/iofiles/MKL25Z4.h:
+
+../Sources/Events.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Types.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Error.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/PE_Const.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/IO_Map.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/SysTick.h:
+
+C:/Users/gabri/OneDrive/Documentos/GitHub/PSI3441/Ex3/Generated_Code/Cpu.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/SysTick_PDD.h:
+
+C:/Freescale/CW\ MCU\ v11.1/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h:
